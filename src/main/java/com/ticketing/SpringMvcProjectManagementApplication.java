0083f67c8151b206aa7b5531bce036bbba20abf4
Petru <nicolaevpetru@outlook.com>
2020-11-21 10:24:56 -0500

    Ticketing Project MVC

---
+++ src/main/java/com/ticketing/SpringMvcProjectManagementApplication.java
@@ -0,0 +1,14 @@
+package com.ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcProjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcProjectManagementApplication.class, args);
+	}
+
+
+}
