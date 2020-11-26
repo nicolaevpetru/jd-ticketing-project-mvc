d36927b15c18a3d2539f65ccefa03ad1e190938f
Petru <nicolaevpetru@outlook.com>
2020-11-25 21:20:54 -0500

    Ticketing Project MVC

--- src/main/java/com/ticketing/controller/ProjectController.java
+++ src/main/java/com/ticketing/controller/ProjectController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -39,6 +40,12 @@ public class ProjectController {
     public String insertProject(ProjectDTO project) {
         projectService.save(project);
         project.setStatus(Status.OPEN);
+        return "redirect:/project/create";
+    }
+
+    @GetMapping("/delete/{projectcode}")
+    public String deleteProject(@PathVariable("projectcode") String projectcode) {
+        projectService.deleteById(projectcode);
         return "redirect:/project/create";
     }
 }
