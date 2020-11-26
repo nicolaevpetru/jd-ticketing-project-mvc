dce5be86724c3bc55cb007e3a8d1c731cbaa8085
Petru <nicolaevpetru@outlook.com>
2020-11-25 19:28:36 -0500

    Ticketing Project MVC - ProjectController added

--- src/main/java/com/ticketing/bootstrap/DataGenerator.java
+++ src/main/java/com/ticketing/bootstrap/DataGenerator.java
@@ -1,5 +1,6 @@
 package com.ticketing.bootstrap;
 
+import com.github.javafaker.Faker;
 import com.ticketing.dto.RoleDTO;
 import com.ticketing.dto.UserDTO;
 import com.ticketing.service.RoleService;
@@ -23,7 +24,7 @@ public class DataGenerator implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
+        Faker faker = new Faker();
         RoleDTO admin = new RoleDTO(1L, "Admin");
         RoleDTO manager = new RoleDTO(2L, "Manager");
         RoleDTO employee = new RoleDTO(3L, "Employee");
@@ -32,16 +33,16 @@ public class DataGenerator implements CommandLineRunner {
         roleService.save(manager);
         roleService.save(employee);
 
-        UserDTO user1 = new UserDTO("John", "Kesy",
-                "admin@gmail.com", "abc", true, "7459684532", admin, Gender.MALE);
-        UserDTO user5 = new UserDTO("John", "Kesy",
-                "admin2@yahoo.com", "abc", true, "7459684532", admin, Gender.MALE);
-        UserDTO user2 = new UserDTO("Delisa",
-                "Norre", "T001@gmail.com", "123", true, "8567412358", manager, Gender.MALE);
-        UserDTO user3 = new UserDTO("Craig", "Jark",
-                "P001@hotmail.com", "123", true, "7777775566", employee, Gender.MALE);
-        UserDTO user4 = new UserDTO("Shaun",
-                "Hayns", "S001@mail.com", "123", true, "3256987412", employee, Gender.MALE);
+        UserDTO user1 = new UserDTO(faker.name().firstName(), faker.name().lastName(),
+                faker.internet().emailAddress(), "abc", true, faker.number().digits(10), admin, Gender.MALE);
+        UserDTO user5 = new UserDTO(faker.name().firstName(), faker.name().lastName(),
+                faker.internet().emailAddress(), "abc", true, faker.number().digits(10), admin, Gender.MALE);
+        UserDTO user2 = new UserDTO(faker.name().firstName(),
+                faker.name().lastName(), faker.internet().emailAddress(), "123", true, faker.number().digits(10), manager, Gender.MALE);
+        UserDTO user3 = new UserDTO(faker.name().firstName(), faker.name().lastName(),
+                faker.internet().emailAddress(), "123", true, faker.number().digits(10), employee, Gender.MALE);
+        UserDTO user4 = new UserDTO(faker.name().firstName(),
+                faker.name().lastName(), faker.internet().emailAddress(), "123", true, faker.number().digits(10), employee, Gender.MALE);
 
 
         userService.save(user1);
---
+++ src/main/java/com/ticketing/controller/ProjectController.java
@@ -0,0 +1,16 @@
+package com.ticketing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/project")
+public class ProjectController {
+
+    @GetMapping("/create")
+    public String projectCreate() {
+
+        return "/project/create";
+    }
+}
